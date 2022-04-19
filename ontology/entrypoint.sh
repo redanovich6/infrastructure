03239209dc41f3330f16ed8378d869b2d4669e70
sostrades-dpeltre <97517335+sostrades-dpeltre@users.noreply.github.com>
2022-04-19 15:58:45 +0200

    Update entrypoint.sh

--- ontology/entrypoint.sh
+++ ontology/entrypoint.sh
@@ -1,4 +1,4 @@
 pwd
 git pull
 pip install -r requirements.txt
-gunicorn sos_ontology.rest_api.api:app --certfile=/etc/ssl/private/tls.crt --keyfile=/etc/ssl/private/tls.key --bind 0.0.0.0:5555 --limit-request-line 0 --timeout 120
+gunicorn sos_ontology.rest_api.api:app --bind 0.0.0.0:5555 --limit-request-line 0 --timeout 120
